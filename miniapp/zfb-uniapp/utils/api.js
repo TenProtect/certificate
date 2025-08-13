8944bd38d536cc11d8a0f4cb59f55838a32b2cfd
xiongzihong <734604253@qq.com>
2025-08-14 01:35:02 +0800

    api结构修改

--- miniapp/zfb-uniapp/utils/api.js
+++ miniapp/zfb-uniapp/utils/api.js
@@ -1,36 +1,67 @@
-const API_BASE = 'http://localhost:8080/v1/mini'
+const API_BASE = 'http://localhost:5000/v1/mini'
 
-export function getCertificates(keyword = '') {
+// 通用请求封装函数
+function request(url, method = 'GET', data = null, options = {}) {
   return new Promise((resolve, reject) => {
-    const url = keyword
-      ? `${API_BASE}/certificate?keyword=${encodeURIComponent(keyword)}`
-      : `${API_BASE}/certificate`
     uni.request({
-      url,
-      method: 'GET',
-      success: (res) => {
-        const list = res.data && res.data.data ? res.data.data : res.data
-        const mapped = (list || []).map(item => ({
-          ...item,
-          specs: {
-            printSize: item.printSize,
-            pixelSize: item.pixelSize,
-            resolution: item.resolution,
-            saveElectronicPhoto: item.saveElectronicPhoto,
-            printLayout: item.printLayout,
-            bgColor: item.bgColor,
-            imageFormat: item.imageFormat,
-            imageFileSize: item.imageFileSize,
-            requirements: item.requirements
-          }
-        }))
-        resolve(mapped)
+      url: `${API_BASE}${url}`,
+      method,
+      data,
+      header: {
+        'Content-Type': 'application/json',
+        ...options.header
       },
-      fail: (err) => reject(err)
+      ...options,
+      success: (res) => {
+        if (res.statusCode >= 200 && res.statusCode < 300) {
+          resolve(res.data)
+        } else {
+          reject(new Error(`请求失败: ${res.statusCode}`))
+        }
+      },
+      fail: (err) => {
+        reject(err)
+      }
     })
   })
 }
 
-export default {
-  getCertificates
+// GET 请求封装
+export function Get(url, params = null, options = {}) {
+  return (additionalParams = {}) => {
+    const finalParams = { ...params, ...additionalParams }
+    const queryString = finalParams && Object.keys(finalParams).length > 0 
+      ? '?' + Object.keys(finalParams).map(key => `${key}=${encodeURIComponent(finalParams[key])}`).join('&')
+      : ''
+    return request(`${url}${queryString}`, 'GET', null, options)
+  }
 }
+
+// POST 请求封装
+export function Post(url, defaultData = null, options = {}) {
+  return (data = {}) => {
+    const finalData = { ...defaultData, ...data }
+    return request(url, 'POST', finalData, options)
+  }
+}
+
+// PUT 请求封装
+export function Put(url, defaultData = null, options = {}) {
+  return (data = {}) => {
+    const finalData = { ...defaultData, ...data }
+    return request(url, 'PUT', finalData, options)
+  }
+}
+
+// DELETE 请求封装
+export function Delete(url, options = {}) {
+  return (params = {}) => {
+    const queryString = params && Object.keys(params).length > 0 
+      ? '?' + Object.keys(params).map(key => `${key}=${encodeURIComponent(params[key])}`).join('&')
+      : ''
+    return request(`${url}${queryString}`, 'DELETE', null, options)
+  }
+}
+
+// API 接口定义
+export const getCertificates = Get('/certificate')
